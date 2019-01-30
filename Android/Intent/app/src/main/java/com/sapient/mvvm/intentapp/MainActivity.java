46b8c1555973200c4ff6638a04a92189302ff09f
Vijay Kumar 2 <vkumar60@sapient.com>
2019-01-30 12:15:00 +0530

    Added intent for Android

---
+++ Android/Intent/app/src/main/java/com/sapient/mvvm/intentapp/MainActivity.java
@@ -0,0 +1,81 @@
+package com.sapient.mvvm.intentapp;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    MyReceiver myReceiver;
+    IntentFilter intentFilter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        myReceiver = new MyReceiver();
+        intentFilter = new IntentFilter("com.sapient.mvvm.intentapp.SOME_ACTION");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(myReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(myReceiver);
+    }
+
+    public void handleNextClick(View view) {
+        Intent myIntent = new Intent(this, SecondActivity.class);
+        this.startActivity(myIntent);
+    }
+}
---
+++ Android/Intent/app/src/main/java/com/sapient/mvvm/intentapp/SecondActivity.java
@@ -0,0 +1,47 @@
+package com.sapient.mvvm.intentapp;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public TextView txtIntentName;
+
+    MyReceiver myReceiver;
+    IntentFilter intentFilter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        txtIntentName = findViewById(R.id.txtIntentName);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        myReceiver = new MyReceiver();
+        intentFilter = new IntentFilter("com.sapient.mvvm.intentapp.SOME_ACTION");
+    }
+
+    public void handleClick(View view) {
+        Intent intent = new Intent("com.sapient.mvvm.intentapp.SOME_ACTION");
+        sendBroadcast(intent);
+    }
+}
